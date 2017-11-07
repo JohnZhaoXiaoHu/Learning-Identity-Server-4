f756a704a9977affb1f4c12a9144a8d1e3aad8f0
Dave Young <solenovex@live.com>
2017-11-07 21:44:04 +0800

    update authorization server with asp.net core identity

---
+++ AuthorizationServer/Configuration/Config.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace AuthorizationServer.Configuration
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("socialnetwork", "社交网络")
+                {
+                    UserClaims = new [] { "email" }
+                }
+            };
+        }
+        
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "socialnetwork",
+                    ClientSecrets = new [] { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] { "socialnetwork" }
+                },
+                new Client
+                {
+                    ClientId = "mvc_code",
+                    ClientName = "MVC Client",
+                    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
+                    RequireConsent = true,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    RedirectUris = { "http://localhost:5002/signin-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "socialnetwork"
+                    },
+                    AllowOfflineAccess = true,
+                    AllowAccessTokensViaBrowser = true
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email()
+            };
+        }
+    }
+}
